a4e016eade723a58e185127bf1036e8214082da6
stufinn <30563561+finst@users.noreply.github.com>
2019-01-23 13:25:10 -0600

    clean up code

--- addWorksheets.js
+++ addWorksheets.js
@@ -11,21 +11,16 @@ function createAllWorkSheets(workbook) {
   for (let cW = 1; cW < 12 + 1; cW++) {
 
     //add a worksheet
-    // addSheet(workbook, cW);
     helpers.addSheet(workbook, cW);
     
     //add dates to each sheet
-    // addDates(cW);
     helpers.addDates(cW);
 
     //add top-row categories
-    // addCategories(cW);
     helpers.addCategories(cW);
 
     //add styles to sheet
-    // addStyles(workbook, cW);
     helpers.addStyles(workbook,cW);
-
   }
 }
 
--- app.js
+++ app.js
@@ -5,19 +5,14 @@ var wb = new xl.Workbook();
 
   // https://stackoverflow.com/questions/6645067/javascript-dynamically-creating-variables-for-loops
 
-//add all of the worksheets to X number
-//passes in the excel workbook object
 //modify it to accept the starting month and auto-generate all of the sheets
-
-// var sheetsToAdd = 12;
-// var startMonth = 0;
 
 addWorkSheets.createAllWorkSheets(wb);
 
 // Add date to file name
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
+// const monthNames = ["January", "February", "March", "April", "May", "June",
+//   "July", "August", "September", "October", "November", "December"
+// ];
 
 // const monthNumbers = ['01','02', '03', '04','05','06','07','08','09','10','11'];
 
