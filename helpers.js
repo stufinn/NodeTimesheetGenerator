d798e65c844ffe44c22008e95731cbe95798543e
stufinn <30563561+finst@users.noreply.github.com>
2019-01-24 14:51:15 -0600

    Remove old months object

--- helpers.js
+++ helpers.js
@@ -74,58 +74,6 @@ const categories = [
   }
 ];
 
-const months = [
-  {
-    name: "January",
-    days: 31
-  },
-  {
-    name: "February",
-    days: 28  // ACCOUNT FOR LEAP YEARS!!
-  },
-  {
-    name: 'March',
-    days: 31
-  },
-  {
-    name: 'April',
-    days: 30
-  },
-  {
-    name: 'May',
-    days: 31
-  },
-  {
-    name: 'June',
-    days: 30
-  },
-  {
-    name: 'July',
-    days: 31
-  },
-  {
-    name: 'August',
-    days: 31
-  },
-  {
-    name: 'September',
-    days: 30
-  },
-  {
-    name: 'October',
-    days: 31
-  },
-  {
-    name: 'November',
-    days: 30
-  },
-  {
-    name: 'December',
-    days: 30
-  }
-];
-
-
 const addSheet = (workbook, cW, datesArray) => {
   worksheet[cW] = workbook.addWorksheet(`${datesArray[cW-1].name}`, {
     'sheetFormat': {
