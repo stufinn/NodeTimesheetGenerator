73ec4520844b134103736f354991dbb35de468cd
stufinn <30563561+finst@users.noreply.github.com>
2019-11-23 14:28:34 -0600

    Prettier auto changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const xl = require('excel4node');
 const addWorkSheets = require('./addWorksheets');
-const fs = require("fs");
+const fs = require('fs');
 
 // create a new instance of a Workbook class
 var wb = new xl.Workbook();
@@ -14,7 +14,8 @@ addWorkSheets.createAllWorkSheets(wb);
 // Add date to file name
 
 let currentDate = new Date();
-let customDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}_${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getSeconds()}`;
+let customDate = `${currentDate.getFullYear()}-${currentDate.getMonth() +
+  1}-${currentDate.getDate()}_${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getSeconds()}`;
 let fileName = `./excelFiles/${customDate}.xlsx`;
 
 // wb.write(`./excelFiles/${currentDate.toDateString()} ${currentDate.toTimeString()}.xlsx`);
