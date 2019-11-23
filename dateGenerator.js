b87d05ec129616c6b0d13b137e1e2525f5f5db55
stufinn <30563561+finst@users.noreply.github.com>
2019-11-23 14:29:22 -0600

    Add comments and Prettier changes

--- dateGenerator.js
+++ dateGenerator.js
@@ -1,5 +1,8 @@
+// To do: write an explanation of what this function does
+// year is an Integer (e.g. 2019)
+// numsheets is the number of sheets that need to be created. It is the length of the paydates array.
+// payDates is an array objects (the pay dates as strings)
 function arrayOfDates(year, numSheets, payDates) {
-
   let startDate = new Date(`Feb 26, ${year}`);
   // console.log(`Start Date: ${startDate}`);
 
@@ -9,21 +12,19 @@ function arrayOfDates(year, numSheets, payDates) {
 
   let monthDay = nextDate.getDate();
 
-
-  for (let j = 0; j < numSheets; j++) { 
+  // Can I use payDates.map() instead?
+  for (let j = 0; j < numSheets; j++) {
     let dateCutoff;
 
     // console.log(`Sheet ${j+1}`);
     // payPeriods[j].dates.push(nextDate);
     // console.log(payPeriods[j]);
 
-  
-    payPeriods.push(
-      {
-        payDate: `${payDates[j].short}`,
-        dates: [],
-      }
-    );
+    // if using .map, can j be the index instead? Wouldn't need it bcause you'd just use the current 'item' in the .map sequence
+    payPeriods.push({
+      payDate: `${payDates[j].short}`,
+      dates: []
+    });
 
     function addToDateArray(dateCutoff) {
       while (monthDay != dateCutoff) {
@@ -31,23 +32,20 @@ function arrayOfDates(year, numSheets, payDates) {
         payPeriods[j].dates.push(nextDate.toDateString());
         nextDate.setDate(nextDate.getDate() + 1);
         monthDay = nextDate.getDate();
-      }  
+      }
     }
 
     if (monthDay >= 26 || monthDay <= 10) {
       dateCutoff = 11;
       addToDateArray(dateCutoff);
-    } else if (monthDay >=11 || monthDay <= 25) {
+    } else if (monthDay >= 11 || monthDay <= 25) {
       dateCutoff = 26;
       addToDateArray(dateCutoff);
     }
-
   }
 
   // console.log(payPeriods);
   return payPeriods;
-
- 
 }
 
-module.exports = {arrayOfDates};
+module.exports = { arrayOfDates };
